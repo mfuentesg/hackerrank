53ece186614e0d9e6320cf9e0e001ffad340fe3f
Marcelo Fuentes <marceloe.fuentes@gmail.com>
2017-06-12 08:26:53 -0400

    master - change len by sum

--- implementation/apple_and_orange.py
+++ implementation/apple_and_orange.py
@@ -4,5 +4,5 @@ s, t = list(map(int, input().strip().split(' ')))
 a, b = list(map(int, input().strip().split(' ')))
 m, n = list(map(int, input().strip().split(' ')))
 
-print(len([1 for x in list(map(int, input().strip().split(' '))) if a + x in range(s, t + 1)]))
-print(len([1 for x in list(map(int, input().strip().split(' '))) if b + x in range(s, t + 1)]))
+print(sum([1 for x in list(map(int, input().strip().split(' '))) if a + x in range(s, t + 1)]))
+print(sum([1 for x in list(map(int, input().strip().split(' '))) if b + x in range(s, t + 1)]))
