fa0c05adab18f88b0df31b992546b5c9205ebea5
Marcelo Fuentes <mefuentesg@falabella.cl>
2017-10-22 20:27:14 -0300

    master - add caesar cipher algorithm

---
+++ algorithms/strings/caesar_cipher.py
@@ -0,0 +1,25 @@
+#!/bin/python3
+
+
+def encrypt_char(c, k):
+    ci = ord(c)
+    l = 'Z' if c.isupper() else 'z'
+    o = 'A' if c.isupper() else 'a'
+
+    if ci + k > ord(l):
+        return chr(ord(o) + (ci + k - ord(l)) - 1)
+
+    return chr(ci + k)
+
+def caesar_cipher(text, key, decrypt = False):
+    if key == 0 or key % 26 == 0:
+        return text
+
+    K = key % 26 if key > 26 else key
+    return ''.join([encrypt_char(x, K) if x.isalpha() else x for x in text])
+
+_ = int(input().strip())
+s = input().strip()
+k = int(input().strip())
+
+print(caesar_cipher(s, k))
